3c0e7db9fb21d963b270f5cf8d1d92f4f19acbb4
지민 <uncensoredgamescript@gmail.com>
2024-10-13 15:59:07 +0900

    Update check_module.py

--- check_module.py
+++ check_module.py
@@ -1,10 +1,16 @@
 import os
+import importlib
+import sys
 import requests
-from pystyle import Colors, Center
-import time
-import check_module
 
-REPO_URL = "https://raw.githubusercontent.com/UncensoredUsers/Jimini-Tool-Py/refs/heads/main/main.py"
+REPO_URL = "https://raw.githubusercontent.com/UncensoredUsers/Jimini-Tool-Py/refs/heads/main/check_module.py"  # 수정 필요
+
+def check_and_install(module_name):
+    try:
+        importlib.import_module(module_name)
+    except ImportError:
+        print(f"{module_name} is not installed. Installing...")
+        os.system(f"{sys.executable} -m pip install {module_name}")
 
 def check_for_update():
     try:
@@ -16,37 +22,21 @@ def check_for_update():
         return None
 
 def update_program(latest_code):
-    with open("main.py", "w", encoding='utf-8') as f:
+    with open("check_module.py", "w", encoding='utf-8') as f:
         f.write(latest_code)
-    print("Update downloaded successfully.")
 
 def main():
-    jimini_ascii_art = """
-         ██╗██╗███╗   ███╗██╗███╗  ██╗██╗  ████████╗ █████╗  █████╗ ██╗
-         ██║██║████╗ ████║██║████╗ ██║██║  ╚══██╔══╝██╔══██╗██╔══██╗██║
-         ██║██║██╔████╔██║██║██╔██╗██║██║     ██║   ██║  ██║██║  ██║██║
-    ██╗  ██║██║██║╚██╔╝██║██║██║╚████║██║     ██║   ██║  ██║██║  ██║██║
-    ╚█████╔╝██║██║ ╚═╝ ██║██║██║ ╚███║██║     ██║   ╚█████╔╝╚█████╔╝███████╗
-     ╚════╝ ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚══╝╚═╝     ╚═╝    ╚════╝  ╚════╝ ╚══════╝
-    """
-    
-    time.sleep(1)
-    print("Checking Modules...")
-    check_module.main()
-    time.sleep(1)
+    modules_to_check = ['requests', 'pystyle']
 
-    print("Checking for updates...")
+    for module in modules_to_check:
+        check_and_install(module)
+
+    print("All Modules Checked")
+
     latest_code = check_for_update()
 
     if latest_code:
         update_program(latest_code)
-
-    print("Starting Program...")
-    time.sleep(1)
-    os.system('cls')
-    print(Colors.light_blue, Center.XCenter(jimini_ascii_art))
-
-    os.system('pause')
 
 if __name__ == "__main__":
     main()
